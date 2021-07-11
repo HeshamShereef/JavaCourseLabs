6c408b45b5ad9ea2f874380ab9f8a0293266087d
HeshamShereef <86895527+HeshamShereef@users.noreply.github.com>
2021-07-11 03:05:11 +0200

    Delete Java Course Labs directory

--- Java Course Labs/Countries_Cities/src/CDao.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.List;
-
-public interface CDao {
-	public List<Country> ReadCountryFromCSV(String fileName) throws FileNotFoundException, IOException;
-	public List<City> ReadCityFromCSV(String fileName) throws IOException;
-
-}
